ad75a07c2499c457506c9ff7aa8094dcd1b5051d
Miracle.Lin <miracle.lin@zeasn.com>
2023-10-30 00:14:51 +0800

    修改logo和应用名，优化结果，显示卦象符号

--- app/src/main/java/com/zds/zhouyi/Gua.java
+++ app/src/main/java/com/zds/zhouyi/Gua.java
@@ -27,11 +27,46 @@ public class Gua implements Serializable {
 
 
     /**
-     * 详情
+     * 编码
+     */
+    String code;
+
+    /**
+     * 卦辞
      */
     String detail;
+    /**
+     * 推断
+     */
+    String inference;
+    /**
+     * 大象
+     */
+    String daXiang;
 
+    /**
+     * 运势
+     */
+    String fortune;
 
+    /**
+     * 爱情
+     */
+    String love;
+
+    /**
+     * 疾病
+     */
+    String disease;
+    /**
+     * 失物
+     */
+    String lost;
+
+    /**
+     * 诉讼
+     */
+    String litigation;
     public int getId() {
         return id;
     }
@@ -88,6 +123,88 @@ public class Gua implements Serializable {
         this.indexGua = indexGua;
         this.descrip = descrip;
         this.detail = detail;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getInference() {
+        return inference;
+    }
+
+    public void setInference(String inference) {
+        this.inference = inference;
+    }
+
+    public String getDaXiang() {
+        return daXiang;
+    }
+
+    public void setDaXiang(String daXiang) {
+        this.daXiang = daXiang;
+    }
+
+    public String getFortune() {
+        return fortune;
+    }
+
+    public void setFortune(String fortune) {
+        this.fortune = fortune;
+    }
+
+    public String getLove() {
+        return love;
+    }
+
+    public void setLove(String love) {
+        this.love = love;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+
+    public String getLost() {
+        return lost;
+    }
+
+    public void setLost(String lost) {
+        this.lost = lost;
+    }
+
+    public String getLitigation() {
+        return litigation;
+    }
+
+    public void setLitigation(String litigation) {
+        this.litigation = litigation;
+    }
+
+    public Gua(int id, String name, int indexGua, String descrip, String code, String detail,
+               String inference, String daXiang, String fortune, String love, String disease,
+               String lost, String litigation) {
+        this.id = id;
+        this.name = name;
+        this.indexGua = indexGua;
+        this.descrip = descrip;
+        this.code = code;
+        this.detail = detail;
+        this.inference = inference;
+        this.daXiang = daXiang;
+        this.fortune = fortune;
+        this.love = love;
+        this.disease = disease;
+        this.lost = lost;
+        this.litigation = litigation;
     }
 
     @Override
--- app/src/main/java/com/zds/zhouyi/MainActivity.java
+++ app/src/main/java/com/zds/zhouyi/MainActivity.java
@@ -5,9 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.animation.Animator;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.method.ScrollingMovementMethod;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -40,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
     int[] result1;
     int[] result2;
 
+    LinearLayout llGua;
+    ImageView gua1;
+    ImageView gua2;
+    ImageView gua3;
+    ImageView gua4;
+    ImageView gua5;
+    ImageView gua6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +70,14 @@ public class MainActivity extends AppCompatActivity {
         coin2 = findViewById(R.id.iv_coin2);
         coin3 = findViewById(R.id.iv_coin3);
         tvSeeAll = findViewById(R.id.tv_see_all);
+        llGua = findViewById(R.id.ll_gua_result);
+        gua1 = findViewById(R.id.iv_gua1);
+        gua2 = findViewById(R.id.iv_gua2);
+        gua3 = findViewById(R.id.iv_gua3);
+        gua4 = findViewById(R.id.iv_gua4);
+        gua5 = findViewById(R.id.iv_gua5);
+        gua6 = findViewById(R.id.iv_gua6);
+
         tvSeeAll.setMovementMethod(new ScrollingMovementMethod());
         lottieAnimationView.setAnimation("loading.json");
         lottieAnimationView.setRepeatCount(3);
@@ -85,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (clickCount == 0) {
                     tvStart.setText("开始预测");
+                    llGua.setVisibility(View.INVISIBLE);
                 } else if (clickCount == 1) {
                     tvStart.setText("请再投一次");
                 }
@@ -102,15 +126,18 @@ public class MainActivity extends AppCompatActivity {
         tvSeeAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                tvSeeAll.setBackground(null);
-                StringBuilder sb = new StringBuilder();
-                for (Gua gua : Const.sixtyFourDiagramsMap) {
-                    sb.append(gua.toString());
-                    sb.append("\n ");
+                if (Objects.equals(tvSeeAll.getText().toString(), "查看全部卦")) {
+                    StringBuilder sb = new StringBuilder();
+                    for (Gua gua : Const.sixtyFourDiagramsMap) {
+                        sb.append(gua.toString());
+                        sb.append("\n ");
+                    }
+                    //sb.setLength(sb.length() - 2);
+                    // 删除最后一个逗号和空格
+                    tvSeeAll.setText(sb.toString());
+                } else {
+                    tvSeeAll.setText("查看全部卦");
                 }
-                //sb.setLength(sb.length() - 2);
-                // 删除最后一个逗号和空格
-                tvSeeAll.setText(sb.toString());
             }
         });
     }
@@ -153,7 +180,17 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             if (nowGua != null) {
-                tvResult.setText("结果是：\n" + "   序号：" + resultNumber + " 第" + nowGua.getIndexGua() + "卦 " + nowGua.getName() + "-- " + nowGua.getDescrip());
+                SpannableString text = new SpannableString("结果是：\n" + "   序号：" + resultNumber + " 第" + nowGua.getIndexGua() + "卦:" + nowGua.getName() + "-- \n" + nowGua.getDescrip());
+                text.setSpan(new ForegroundColorSpan(Color.RED), text.toString().indexOf("卦:") + 2, text.toString().indexOf("--"), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                text.setSpan(new AbsoluteSizeSpan(getResources().getDimensionPixelSize(R.dimen.dp_48), true), text.toString().indexOf("卦:") + 2, text.toString().indexOf("--"), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                tvResult.setText(text);
+                llGua.setVisibility(View.VISIBLE);
+                gua1.setBackgroundResource(result1[0] == 1 ? R.color.black : R.drawable.yin);
+                gua2.setBackgroundResource(result1[1] == 1 ? R.color.black : R.drawable.yin);
+                gua3.setBackgroundResource(result1[2] == 1 ? R.color.black : R.drawable.yin);
+                gua4.setBackgroundResource(result2[0] == 1 ? R.color.black : R.drawable.yin);
+                gua5.setBackgroundResource(result2[1] == 1 ? R.color.black : R.drawable.yin);
+                gua6.setBackgroundResource(result2[2] == 1 ? R.color.black : R.drawable.yin);
             }
         }
     }
